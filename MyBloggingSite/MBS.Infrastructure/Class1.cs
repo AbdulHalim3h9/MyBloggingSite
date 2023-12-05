62388e905f50b5d3c429f199d15f1c992ee44ff8
Abdul Halim Khan <abdulhalimkhan271@gmail.com>
2023-12-05 20:38:29 +0600

    Entities added

---
+++ MyBloggingSite/MBS.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace MBS.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ MyBloggingSite/MBS.Infrastructure/Entities/Blog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBS.Infrastructure.Entities
+{
+    public class Blog
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Article { get; set; }
+    }
+}
