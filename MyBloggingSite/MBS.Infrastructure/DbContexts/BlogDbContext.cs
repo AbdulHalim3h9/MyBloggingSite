0fa83b6ba6e6bbc8d40de5ed34e0cf9c12eb5d94
Abdul Halim Khan <abdulhalimkhan271@gmail.com>
2023-12-25 14:21:17 +0600

    Working DbContext Added
    # Conflicts:
    #	MyBloggingSite/MBS.Infrastructure/DbContexts/BlogDbContext.cs

--- MyBloggingSite/MBS.Infrastructure/DbContexts/BlogDbContext.cs
+++ MyBloggingSite/MBS.Infrastructure/DbContexts/BlogDbContext.cs
@@ -13,10 +13,10 @@ namespace MBS.Infrastructure.DbContexts
     public class BlogDbContext : IdentityDbContext<IdentityUser>
     {
         public DbSet<Blog> Blogs { get; set; }
-        public BlogDbContext(DbContextOptions<BlogDbContext> options)
-            : base(options)
-        {
-        }
+        //public BlogDbContext(DbContextOptions<BlogDbContext> options)
+        //    : base(options)
+        //{
+        //}
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=aspnet-MBS.Web-e96c3ddc-91b6-40ac-89e0-12a4ff31b2a9;Trusted_Connection=True;MultipleActiveResultSets=true");
